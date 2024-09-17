4f22131e83e9f6d70cc94d47a0dde1e25faf2d1c
Shiluco <hirap1939@gmail.com>
2024-09-17 12:17:20 +0900

    feat:ホームのアニメーション

---
+++ src/components/Organisms/Welcome.tsx
@@ -0,0 +1,78 @@
+import { Box, Typography } from "@mui/material";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import welcomeImage from "../../../public/photos/Portrait/02.jpg";
+
+const Welcome = () => {
+  const welcomeImageRef = useRef(null);
+  const sideTextRef1 = useRef(null);
+  const sideTextRef2 = useRef(null);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+
+    timeline.fromTo(
+      welcomeImageRef.current,
+      { opacity: 0.2 },
+      { opacity: 1, duration: 1 }
+    );
+
+    timeline.fromTo(
+      sideTextRef1.current,
+      { clipPath: "inset(0% 0% 100% 0)" },
+      { clipPath: "inset(0% 0% 0% 0)", duration: 1 },
+      
+    );
+
+    
+    timeline.fromTo(
+      sideTextRef2.current,
+      { clipPath: "inset(0 0 100% 0)" },
+      { clipPath: "inset(0 0 0% 0)", duration: 1 },
+      "<"
+    );
+  }, []);
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-evenly"
+      alignItems="center"
+      height="90vh"
+    >
+      <Typography
+        variant="h1"
+        ref={sideTextRef1}
+        sx={{
+          writingMode: "vertical-rl",
+          transform: "rotate(180deg)",
+        }}
+      >
+        Shiluco
+      </Typography>
+
+      <img
+        id="welcomeImage"
+        src={welcomeImage}
+        alt="welcomeImage"
+        ref={welcomeImageRef}
+        style={{
+          height: "90%",
+          borderRadius: "2px",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
+        }}
+      />
+
+      <Typography
+        variant="h1"
+        ref={sideTextRef2}
+        style={{ writingMode: "vertical-lr" }}
+      >
+        Photos
+      </Typography>
+    </Box>
+  );
+};
+
+export default Welcome;
--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,44 +1,13 @@
-// library
-import { Box, Typography } from "@mui/material";
-// file
-import topimage from "../../../public/photos/Portrait/02.jpg";
+//file
+import Welcome from "../Organisms/Welcome";
 import Header from "../templates/Hedder";
+
 
 const Top = () => {
   return (
     <>
       <Header text="Top" />
-
-      <Box
-        display="flex"
-        justifyContent="space-evenly"
-        alignItems="center"
-        height="90vh"
-      >
-        <Typography
-          variant="h1"
-          style={{
-            writingMode: "vertical-rl",
-            transform: "rotate(180deg)",
-          }}
-        >
-          Shiluco
-        </Typography>
-
-        <img
-          src={topimage}
-          alt="topimage"
-          style={{
-            height: "90%",
-            borderRadius: "2px",
-            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
-          }}
-        />
-
-        <Typography variant="h1" style={{ writingMode: "vertical-lr" }}>
-          Photos
-        </Typography>
-      </Box>
+      <Welcome/>
     </>
   );
 };
