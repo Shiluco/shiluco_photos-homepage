47f1d20b63363154e74781c10149b664cca3df38
Shiluco <hirap1939@gmail.com>
2024-10-14 12:07:44 +0900

    feat:モーダル

--- src/components/pages/EditPhoto.tsx
+++ src/components/pages/EditPhoto.tsx
@@ -1,35 +1,49 @@
 import { Box } from "@mui/material";
 import MenuBar from "../Organisms/MenuBar";
 import ImgSort from "../Organisms/imgSort";
+import AddPhotoButton from "../atoms/addPhotoButton";
+import { useState } from "react";
+import AddPhotoModal from "../templates/addPhotoModal";
 
+const EditPhoto = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <Box
+        zIndex={0}
+        sx={{
+          height: "100vh",
+          width: "100vw",
+          padding: "10px",
+          backgroundColor: "rgba(0,0,0,0.1)",
+        }}
+      >
+        <Box display={"flex"}>
+          <Box paddingRight={3}>
+            <MenuBar />
+          </Box>
 
-const EditPhoto = () => (
-  <>
-    <Box
-      sx={{
-        height: "100vh",
-        width: "100vw",
-        padding: "10px",
-        backgroundColor: "rgba(0,0,0,0.1)",
-      }}
-    >
-      <Box display={"flex"}>
-        <Box>
-          <MenuBar />
-        </Box>
-
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexGrow={1}
-        >
-          <Box padding={3}/>
-          <ImgSort/>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <ImgSort />
+            <Box marginTop={2}>
+              <AddPhotoButton
+                isModalOpen={isModalOpen}
+                setIsModalOpen={setIsModalOpen}
+              />
+            </Box>
+          </Box>
         </Box>
       </Box>
-    </Box>
-  </>
-);
+      {isModalOpen && (
+        <>
+          <AddPhotoModal
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+          />
+        </>
+      )}
+    </>
+  );
+};
 
 export default EditPhoto;
